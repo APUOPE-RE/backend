1dbdf998469ee36735501415318ef40b00cec5be
hmmoal <mohammed.al-gburi@tuni.fi>
2024-09-11 20:40:43 +0300

    Initialization Spring Boot backend

---
+++ src/main/java/com/apuope/apuope_re/ApuopeReApplication.java
@@ -0,0 +1,14 @@
+package com.apuope.apuope_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuopeReApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApuopeReApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apuope/apuope_re/ApuopeReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apuope.apuope_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApuopeReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
