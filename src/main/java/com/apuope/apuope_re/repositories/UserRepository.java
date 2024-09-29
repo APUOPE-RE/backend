5097147953ce1c14cea807be1b52c917acad4dc8
williamirva <williamirva@gmail.com>
2024-09-29 15:43:34 +0300

    Issue6 fetch user information with JOOQ-query

--- src/main/java/com/apuope/apuope_re/repositories/UserRepository.java
+++ src/main/java/com/apuope/apuope_re/repositories/UserRepository.java
@@ -1,10 +1,20 @@
 package com.apuope.apuope_re.repositories;
 
 import com.apuope.apuope_re.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.apuope.apuope_re.jooq.tables.Users;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+@Repository
+public class UserRepository {
+    @Autowired
+    public UserRepository(){};
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmailAndPasswordHash(String email, String passwordHash);
+    public Optional<User> findByEmailAndPasswordHash(String email, String passwordHash, DSLContext context){
+        return context.select().from(Users.USERS)
+                .where(Users.USERS.EMAIL.eq(email).and(Users.USERS.PASSWORD_HASH.eq(passwordHash)))
+                .fetchOptionalInto(User.class);
+    }
 }
--- src/main/java/com/apuope/apuope_re/services/LogInService.java
+++ src/main/java/com/apuope/apuope_re/services/LogInService.java
@@ -3,21 +3,23 @@ package com.apuope.apuope_re.services;
 import com.apuope.apuope_re.entities.User;
 import com.apuope.apuope_re.repositories.UserRepository;
 import com.apuope.apuope_re.dto.UserCredentials;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
 public class LogInService {
+    private final DSLContext dslContext;
     private final UserRepository userRepository;
-
-    public LogInService(UserRepository userRepository) {
+    public LogInService(DSLContext dslContext, UserRepository userRepository) {
+        this.dslContext = dslContext;
         this.userRepository = userRepository;
     }
 
     public boolean validateUser(UserCredentials userCredentials) {
         Optional<User> userOpt =
                 userRepository.findByEmailAndPasswordHash(userCredentials.getEmail(),
-                        userCredentials.getPasswordHash());
+                        userCredentials.getPasswordHash(), this.dslContext);
 
         if (userOpt.isPresent()) {
             return true;
