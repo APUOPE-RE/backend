a981aa440c1b16b475f80d733f38032a0edd6e76
williamirva <williamirva@gmail.com>
2024-09-29 15:43:51 +0300

    Issue6 update login tests

--- src/test/java/com/apuope/apuope_re/services/LogInServiceTest.java
+++ src/test/java/com/apuope/apuope_re/services/LogInServiceTest.java
@@ -1,47 +1,48 @@
 package com.apuope.apuope_re.services;
 
-import com.apuope.apuope_re.entities.User;
 import com.apuope.apuope_re.repositories.UserRepository;
 import com.apuope.apuope_re.dto.UserCredentials;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.jooq.DSLContext;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-
-class LogInServiceTest {
-    @Mock
+@SpringBootTest
+@ActiveProfiles("test")
+public class LogInServiceTest {
+    public static String TEST_EMAIL = "test@success.com";
+    public static String TEST_PASSWORD = "password123";
+    @Autowired
+    DSLContext dslContext;
+    @Autowired
     private UserRepository userRepository;
-
-    @InjectMocks
+    @Autowired
     private LogInService logInService;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        TestDataGenerator.insertTestUser(dslContext, TEST_EMAIL, TEST_PASSWORD);
+    }
+
+    @AfterEach
+    void tearDown() {
+        TestDataGenerator.deleteTestUsers(dslContext);
     }
 
     @Test
     void testLoginSuccess() {
-        User mockUser = Mockito.mock(User.class);
-        when(userRepository.findByEmailAndPasswordHash("test@success.com", "password123")).thenReturn(Optional.of(mockUser));
-
-        UserCredentials userCredentials = new UserCredentials("test@success.com", "password123");
+        UserCredentials userCredentials = new UserCredentials(TEST_EMAIL, TEST_PASSWORD);
         boolean result = logInService.validateUser(userCredentials);
         assertTrue(result, "Login successful");
     }
 
     @Test
     void testLoginFailure() {
-        User mockUser = new User();
-        when(userRepository.findByEmailAndPasswordHash("test@failure.com", "password123")).thenReturn(Optional.empty());
-
         UserCredentials userCredentials = new UserCredentials("test@failure.com", "password123");
         boolean result = logInService.validateUser(userCredentials);
         assertFalse(result, "Login unsuccessful");
