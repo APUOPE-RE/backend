7be795659e3a2f2cff2f610cf3657e3a6b38edc3
williamirva <williamirva@gmail.com>
2024-10-31 20:31:44 +0200

    issue-11 connect backend to LLM api

--- src/main/java/com/apuope/apuope_re/controllers/ChatbotController.java
+++ src/main/java/com/apuope/apuope_re/controllers/ChatbotController.java
@@ -3,7 +3,7 @@ package com.apuope.apuope_re.controllers;
 import com.apuope.apuope_re.dto.ChatRequestData;
 import com.apuope.apuope_re.dto.ResponseData;
 import com.apuope.apuope_re.services.ChatbotService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("/api")
 public class ChatbotController {
-    @Autowired
-    private ChatbotService chatbotService;
+    private final ChatbotService chatbotService;
+
+    public ChatbotController(ChatbotService chatbotService) {
+        this.chatbotService = chatbotService;
+    }
 
     @PostMapping(value = "/chatBot")
-    public ResponseEntity<ResponseData<String>> validateUser(@RequestBody ChatRequestData input) {
-        return ResponseEntity.ok(chatbotService.sendRequest(input));
+    public ResponseEntity<ResponseData<String>> validateUser(@RequestBody ChatRequestData input) throws JsonProcessingException {
+        return ResponseEntity.ok(this.chatbotService.sendRequest(input));
     }
 }
