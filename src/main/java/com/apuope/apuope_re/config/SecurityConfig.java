37ac6190e90f78d86d67b2efc30da4513b6f0375
Emil Selroos <41218215+emilselroos@users.noreply.github.com>
2024-12-03 22:26:41 +0200

    Test modified CORS settings

--- src/main/java/com/apuope/apuope_re/config/SecurityConfig.java
+++ src/main/java/com/apuope/apuope_re/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://130.230.52.194:8000"));
+        configuration.setAllowedOrigins(
+                Arrays.asList("http://localhost:3000", "http://130.230.52.194", "https://130.230.52.194"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
