94e7807ff844a3e834df6eecacc8350db00e0c15
williamirva <williamirva@gmail.com>
2024-12-03 22:14:30 +0200

    remove crossorigin annotation

--- src/main/java/com/apuope/apuope_re/controllers/RegistrationController.java
+++ src/main/java/com/apuope/apuope_re/controllers/RegistrationController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/api")
 public class RegistrationController {
     @Autowired
